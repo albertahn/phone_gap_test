6750daad0be8de9e327b6d1e774d9e77631a5e9b
Albert <contendera@gmail.com>
2017-10-02 22:35:20 -0700

    added firebase plist

--- PushNoios/platforms/ios/platform_www/cordova_plugins.js
+++ PushNoios/platforms/ios/platform_www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-firebase": "0.1.24"
+    "cordova-plugin-firebase": "0.1.24",
+    "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
