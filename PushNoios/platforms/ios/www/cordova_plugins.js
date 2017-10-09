4ebcdf9063044c9b8579b1c5bed898718e9e18e1
Albert <contendera@gmail.com>
2017-10-09 13:06:11 -0700

    all works with firebase : https://www.youtube.com/watch?v=JsWHzU1DxjM

--- PushNoios/platforms/ios/www/cordova_plugins.js
+++ PushNoios/platforms/ios/www/cordova_plugins.js
@@ -1,28 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-firebase.FirebasePlugin",
-        "file": "plugins/cordova-plugin-firebase/www/firebase.js",
-        "pluginId": "cordova-plugin-firebase",
-        "clobbers": [
-            "FirebasePlugin"
-        ]
-    },
-    {
         "id": "cordova-plugin-dialogs.notification",
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-firebase": "0.1.24",
+    "cordova-plugin-dialogs": "1.3.3",
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-dialogs": "1.3.3"
+    "phonegap-plugin-push": "2.0.0"
 };
 // BOTTOM OF METADATA
 });
--- PushNoios/platforms/ios/www/js/index.js
+++ PushNoios/platforms/ios/www/js/index.js
@@ -27,7 +27,120 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        window.FirebasePlugin.grantPermission();
+        //window.FirebasePlugin.grantPermission();
+
+        var push = PushNotification.init({
+	android: {
+	},
+    browser: {
+        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+    },
+	ios: {
+		alert: "true",
+		badge: "true",
+		sound: "true"
+	},
+	windows: {}
+});
+
+push.on('registration', function(data) {
+	// data.registrationId
+
+  navigator.notification.alert(
+      ' data.registrationId: '+ data.registrationId,  // message
+      alertDismissed,         // callback
+      'YOu got albert registration!!!',            // title
+      'Done'                  // buttonName
+  );//noti alert
+});
+
+push.on('notification', function(data) {
+	// data.message,
+	// data.title,
+	// data.count,
+	// data.sound,
+	// data.image,
+	// data.additionalData
+console.log("data:"+data);
+  navigator.notification.alert(
+      ' data.registrationId: '+data,  // message
+      alertDismissed,         // callback
+      'YOu got token!!!',            // title
+      'Done'                  // buttonName
+  );//noti alert
+
+});
+
+push.on('error', function(e) {
+  navigator.notification.alert(
+      ' e.message: '+e.message,  // message
+      alertDismissed,         // callback
+      'YOu got token!!!',            // title
+      'Done'                  // buttonName
+  );//noti alert
+	// e.message
+});
+/*
+        window.FirebasePlugin.getToken(function(token) {
+            // save this server-side and use it to push notifications to this device
+            console.log(token);
+
+            navigator.notification.alert(
+                'albert YOu got token!!!'+token,  // message
+                alertDismissed,         // callback
+                'YOu got token!!!',            // title
+                'Done'                  // buttonName
+            );
+
+
+
+        }, function(error) {
+            console.error(error);
+        });
+
+        window.FirebasePlugin.logEvent("select_content", {content_type: "page_view", item_id: "home"});
+
+        window.FirebasePlugin.setScreenName("Home");
+        window.FirebasePlugin.setUserId("albert_id");
+
+        window.FirebasePlugin.onTokenRefresh(function(token) {
+            // save this server-side and use it to push notifications to this device
+            console.log(token);
+
+            navigator.notification.alert(
+                'YOu got onTokenRefresh token!!!',  // message
+                alertDismissed,         // callback
+                'YOu got onTokenRefresh!!!',            // title
+                'Done'                  // buttonName
+            );
+
+        }, function(error) {
+            console.error(error);
+        });
+
+        window.FirebasePlugin.onNotificationOpen(function(notification) {
+            console.log(notification);
+
+            navigator.notification.alert(
+                'YOu got notification!!!',  // message
+                alertDismissed,         // callback
+                'YOu got notification!!!',            // title
+                'Done'                  // buttonName
+            );
+
+        }, function(error) {
+            console.error(error);
+        });
+
+        window.FirebasePlugin.hasPermission(function(data){
+            console.log(data.isEnabled);
+            navigator.notification.alert(
+                'YOu got permision!!!',  // message
+                alertDismissed,         // callback
+                'YOu got permision!!!',            // title
+                'Done'                  // buttonName
+            );
+        });
 
         FCMPlugin.getToken(function(token){
             //alert(token);
@@ -40,6 +153,21 @@ var app = {
                     );
         });
 
+        FCMPlugin.onNotification(function(data){
+            if(data.wasTapped){
+              //Notification was received on device tray and tapped by the user.
+              navigator.notification.alert(
+            'onNotification!',  // message
+            alertDismissed,         // callback
+            'onNotification',            // title
+            'Done'                  // buttonName
+        );
+            }else{
+              //Notification was received in foreground. Maybe the user needs to be notified.
+              // alert(JSON.stringify(data));
+            }
+          });
+*/
         navigator.notification.alert(
             'You are the winner!',  // message
             alertDismissed,         // callback
